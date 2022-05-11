0d46881f9f4b149e8d3521d1411625cb8c27af2d
Kyle Chen <kylechen@amd.com>
2022-05-11 15:42:21 -0700

    update for rocm5.2

--- common/install_miopen.sh
+++ common/install_miopen.sh
@@ -62,7 +62,7 @@ MIOPEN_CMAKE_COMMON_FLAGS="
 if [[ $ROCM_INT -ge 50200 ]]; then
     MIOPEN_CMAKE_DB_FLAGS="-DMIOPEN_EMBED_DB=gfx900_56;gfx906_60;gfx90878;gfx90a6e;gfx1030_36"
     MIOPEN_BRANCH="release/rocm-rel-5.2-staging"
-if [[ $ROCM_INT -ge 50100 ]]; then
+elif [[ $ROCM_INT -ge 50100 ]]; then
     MIOPEN_CMAKE_DB_FLAGS="-DMIOPEN_EMBED_DB=gfx900_56;gfx906_60;gfx90878;gfx90a6e;gfx1030_36"
     MIOPEN_BRANCH="release/rocm-rel-5.1-staging"
 elif [[ $ROCM_INT -ge 50000 ]]; then
